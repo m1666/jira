8b04bdd651c06604af9d3291fc4a9c7cf1a05fc2
Mao <160663534@qq.com>
2021-07-20 00:27:45 +0800

    feat: 添加工程列表

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,3 +1,37 @@
+import React from "react";
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import { useEffect, useState } from "react";
+import { cleanObject } from "../../utils";
+import * as qs from "qs";
+
+const apiUrl =  process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
-  return <div></div>;
+  const [param, setParam] = useState({
+    name: "",
+    personId: ""
+  });
+  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [param]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async response => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, [users]);
+
+  return <div>
+    <SearchPanel users={users} param={param} setParam={setParam} />
+    <List users={users} list={list}/>
+  </div>;
 }
--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,3 +1,37 @@
-export const SearchPanel = () => {
-  return <form action=""></form>;
+import React from "react";
+import { useEffect, useState } from "react";
+
+export const SearchPanel = ({ users, param, setParam }) => {
+  return (
+    <form>
+      <div>
+        <input
+          type="text"
+          value={param.name}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              name: evt.target.value,
+            })
+          }
+        />
+        <select
+          value={param.personId}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              personId: evt.target.value,
+            })
+          }
+        >
+          <option value={""}>负责人</option>
+          {users.map((user) => (
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </form>
+  );
 };
